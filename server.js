d2d1ffdd24329b58e12f4cf025833c717d75e15f
Nick Conklin <nconkl1@outlook.com>
2018-10-09 17:12:02 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 var port = 3000;
 
 app.get("/", function (req, res) {
-  res.redirect("index");
+  res.render("index");
 });
 
 app.get("/contact", function (req, res) {
