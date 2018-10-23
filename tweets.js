6616d4f7479cfe47c23468c9adcf2492d9b9569b
Nick Conklin <nconkl1@outlook.com>
2018-10-22 23:23:15 -0400

    Updated tweets.js using the twitter npm package

--- tweets.js
+++ tweets.js
@@ -1,93 +1,26 @@
-let request = require('request');
 let config = require('./config.json');
-let fetch = require('node-fetch');
+var Twitter = require('twitter');
 
-const timestamp = () => {
-    var d = new Date();
-    var n = d.getTime();
-    return n;
-}
-
-let auth = {
-    oauth_consumer_key: config.consumerKey,
+var client = new Twitter({
+    consumer_key: config.consumerKey,
     consumer_secret: config.consumerKeySecret,
-    oauth_token: config.accessToken,
-    token_secret: config.accessTokenSecret,
-    oauth_version: "1.0",
-    oauth_signature_method: "HMAC-SHA1",
-    oauth_timestamp: timestamp()
+    access_token_key: config.accessToken,
+    access_token_secret: config.accessTokenSecret
+});
 
-}
-
-let search_config = {
-    url: config.searchURL
-}
-
-let query = {
-    "query": "#littlehousecreative @Nicole_LHC omit_script=true"
-}
-
-let request_options = {
-    //POST form with "body: query" below
-    url: search_config['url'],
-
-    oauth: auth,
-    json: true,
-    headers: {
-        'content-type': 'application/json',
-        'User-Agent': 'OAuth gem v0.4.4',
-        'OAuth': auth
-    },
-    body: query
-}
+var params = {
+    screen_name: 'Nicole_LHC'
+};
 
 function getData() {
-    return fetch(request_options['url'], {
-            method: 'POST',
-            headers: request_options['headers'],
-            body: request_options['body']
-        })
-        .then(response => {
-            console.log(response.json());
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-            return data;
-        });
-}
-
-function getPost(request_options) {
-    fetch(request_options['url'], {
-            method: 'POST',
-            headers: request_options['headers'],
-            body: request_options['body']
-        })
-        .then(res => {
-            return new Promise((resolve, reject) => {
-                res.body.pipe(dest);
-                res.body.on('error', err => {
-                    reject(err);
-                });
-                dest.on('finish', () => {
-                    resolve(res => res.json());
-                });
-                dest.on('error', err => {
-                    reject(err);
-                });
-            });
-        });
-}
-
-// POST request
-request.post(request_options, function (error, response, body) {
-    if (error) {
-        console.log('Error making search request');
+    client.get('search/tweets', {
+        q: '#littlehousecreative'
+    }, function (error, tweets, response) {
+        if (!error) {
+            console.log(tweets);
+        }
         console.log(error);
-        return;
-    }
-
-    return body;
-});
+    });
+}
 
 module.exports.getData = getData;
